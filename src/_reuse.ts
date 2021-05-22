bdcb964439e219b69193ec0f0c39be00619f514d
MszBednarski <msz.bednarski@gmail.com>
2021-05-22 13:36:58 +0200

    add get block number

--- src/_reuse.ts
+++ src/_reuse.ts
@@ -2,6 +2,25 @@ import { getZil } from "./zilSetup";
 import { createValParam, callContract, getContractState } from "./utill";
 import { BN } from "@zilliqa-js/zilliqa";
 
+export async function getBlockNumber(secondsToAdd: number): Promise<string> {
+  const zil = await getZil();
+  const txblock = await zil.blockchain.getLatestTxBlock();
+  const txblockRate = await zil.blockchain.getTxBlockRate();
+  if (typeof txblock.result == "undefined") {
+    throw new Error("Couldn't get tx block");
+  }
+  if (typeof txblockRate.result == "undefined") {
+    throw new Error("Couldn't get tx block rate");
+  }
+  const curBlockNumber = parseInt(txblock.result.header.BlockNum);
+  const secondsPerTxBlock = 1 / txblockRate.result;
+  const res =
+    "" + (curBlockNumber + Math.round(secondsToAdd / secondsPerTxBlock));
+  console.log(`Current block number: ${curBlockNumber}`);
+  console.log(`Returned Block number: ${res}`);
+  return res;
+}
+
 export async function UpdateAdmin(a: string, newAdmin: string) {
   const zil = await getZil();
   const tx = await callContract(
